5648e0ce63cea7d087929048d8bcfca7a0fa6108
ankit pwr <ankitpam321@gmail.com>
2025-07-20 13:25:17 +0530

    add toolbox

--- apps/frontend/components/button.tsx
+++ apps/frontend/components/button.tsx
@@ -1,19 +1,35 @@
 "use client";
-import React from "react";
+import React, { ReactNode } from "react";
 
+const VarientStyle = {
+  primary: "px-3 py-3 rounded-md bg-purple-400 text-white",
+  secondary: "p-2.5 rounded-md",
+};
+
+const Size = {
+  sm: `w-24 h-8 rounded-md   gap-1 text-md`,
+  md: ` rounded-md  gap-2 text-lg `,
+};
 export default function Button({
-  title,
+  varient,
+  size,
+  children,
+  isActive,
   onClickhandler,
 }: {
-  title: string;
+  title?: string;
+  children?: ReactNode;
   onClickhandler?: React.MouseEventHandler<HTMLButtonElement>;
+  varient: keyof typeof VarientStyle;
+  size: keyof typeof Size;
+  isActive: boolean;
 }) {
   return (
     <button
       onClick={onClickhandler}
-      className="px-3 py-3 rounded-md bg-purple-400 text-white"
+      className={`${VarientStyle[varient]} ${Size[size]} flex items-center justify-center hover:bg-purple-200 cursor-pointer ${isActive && "bg-purple-200"}`}
     >
-      {title}
+      {children}
     </button>
   );
 }
---
+++ apps/frontend/components/tools.tsx
@@ -0,0 +1,114 @@
+import React from "react";
+import Button from "./button";
+import { Tool } from "@/canvas/types/types";
+import {
+  Square,
+  Hand,
+  MousePointer,
+  Diamond,
+  Circle,
+  MoveRight,
+  Minus,
+  Pencil,
+  Eraser,
+  TypeOutline,
+} from "lucide-react";
+export default function Tools({
+  setTool,
+  currentTool,
+}: {
+  setTool: React.Dispatch<React.SetStateAction<Tool>>;
+  currentTool: Tool;
+}) {
+  const handleTool = (tool: Tool) => {
+    console.log(`tool is being set up ${tool}`);
+    setTool(tool);
+  };
+  return (
+    <div className="fixed top-5 left-1/2 -translate-x-1/2 px-1 py-1 flex gap-2 rounded-md shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] ">
+      <Button
+        isActive={currentTool == "Pan" ? true : false}
+        onClickhandler={() => handleTool("Pan")}
+        varient={"secondary"}
+        size={"md"}
+      >
+        <Hand color="#1b1b1f" size={20} strokeWidth={1.5} />{" "}
+      </Button>
+      <Button
+        isActive={currentTool == "Select" ? true : false}
+        onClickhandler={() => handleTool("Select")}
+        varient={"secondary"}
+        size={"md"}
+      >
+        {" "}
+        <MousePointer color="#1b1b1f" size={20} strokeWidth={1.5} />
+      </Button>
+      <Button
+        isActive={currentTool == "Rectangle" ? true : false}
+        onClickhandler={() => handleTool("Rectangle")}
+        size={"md"}
+        varient={"secondary"}
+      >
+        {" "}
+        <Square color="#1b1b1f" size={20} strokeWidth={1.5} />{" "}
+      </Button>
+      <Button
+        isActive={currentTool == "Diamond" ? true : false}
+        onClickhandler={() => handleTool("Diamond")}
+        varient={"secondary"}
+        size={"md"}
+      >
+        <Diamond color="#1b1b1f" size={20} strokeWidth={1.5} />
+      </Button>
+      <Button
+        isActive={currentTool == "Ellipse" ? true : false}
+        onClickhandler={() => handleTool("Ellipse")}
+        varient={"secondary"}
+        size={"md"}
+      >
+        <Circle color="#1b1b1f" size={20} strokeWidth={1.5} />
+      </Button>
+      <Button
+        isActive={currentTool == "Arrow" ? true : false}
+        onClickhandler={() => handleTool("Arrow")}
+        varient={"secondary"}
+        size={"md"}
+      >
+        <MoveRight color="#1b1b1f" size={20} strokeWidth={1.5} />
+      </Button>
+      <Button
+        isActive={currentTool == "Line" ? true : false}
+        onClickhandler={() => handleTool("Line")}
+        varient={"secondary"}
+        size={"md"}
+      >
+        <Minus color="#1b1b1f" size={20} strokeWidth={1.5} />
+      </Button>
+
+      <Button
+        isActive={currentTool == "Pencil" ? true : false}
+        onClickhandler={() => handleTool("Pencil")}
+        varient={"secondary"}
+        size={"md"}
+      >
+        <Pencil color="#1b1b1f" size={20} strokeWidth={1.5} />
+      </Button>
+      <Button
+        isActive={currentTool == "Eraser" ? true : false}
+        onClickhandler={() => handleTool("Eraser")}
+        varient={"secondary"}
+        size={"md"}
+      >
+        <Eraser color="#1b1b1f" size={20} strokeWidth={1.5} />
+      </Button>
+      <Button
+        isActive={currentTool == "Text" ? true : false}
+        onClickhandler={() => handleTool("Text")}
+        varient={"secondary"}
+        size={"md"}
+      >
+        <TypeOutline color="#1b1b1f" size={20} strokeWidth={1.5} />
+      </Button>
+    </div>
+  );
+}
